4d8788387d2a1a47fdf69b7583a624d7245f9e2d
gretchencollins <gretchen.collins@umconnect.umt.edu>
2026-03-12 19:38:59 -0600

    Homework 9

---
+++ Homework 9/sketch.js
@@ -0,0 +1,30 @@
+function setup()
+            {
+                createCanvas(500,600);
+            }
+            function draw()
+            {
+                background(70,25,100);
+                fill(65,120,50)
+                ellipse(250,200,175,200)
+                fill(100,10,10)
+                ellipse(290,175,50,25)
+                fill(100,10,10)
+                ellipse(210,175,50,25)
+                fill(40,50,90)
+                rect(210,300,80,90)
+                fill(750,70,10)
+                triangle(250,500,100,380,400,380)
+                strokeWeight(10)
+                point(200,220)
+                point(300,220)
+                line(75,525,145,450)
+                line(100,525,165,460)
+                line(125,525,185,470)
+                textSize(22)
+                text('Gretchen Collins', 332,590)
+                fill(200,200,43)
+                textSize(45)
+                text('My Portrait', 10,42)
+            
+            }
